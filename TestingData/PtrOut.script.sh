ae2428bc032933b4fd8b612af6523c27cc73c85d
Walid Korani <wkorani@hudsonalpha.org>
2025-04-24 11:47:42 -0500

    Delete TestingData directory

--- TestingData/PtrOut.script.sh
+++
@@ -1,7 +0,0 @@
-
-Ptr.rename chr01.fa ChrQuery03
-Ptr.rename chr02.fa ChrQuery03
-Ptr.split chr03.fa 2056058
-Ptr.RevComp chr03_P1.fa
-Ptr.join chr03_P1_RC.fa chr03_P2.fa
-Ptr.rename chr03_P1_RC_J_chr03_P2.fa ChrQuery03
